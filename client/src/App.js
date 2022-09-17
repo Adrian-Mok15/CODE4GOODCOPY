0f1315c01de2554ef07e3aa0b3481c44a3158927
Michael Shen <michaelshen85@gmail.com>
2022-09-16 22:14:19 -0400

    Add signup form

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ function App() {
         </Routes>
       </Router>
     </div>
-  );
+  )
 }
 
 export default App;
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from 'react';
+import './SignUp.css';
 
-const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
+class SignupForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      username: '',
+      password: '',
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.update = this.update.bind(this);
+  }
+
+  handleSubmit() {
+    // store user in database
+  }
+
+  update(field) {
+    return e => this.setState({[field]: e.target.value});
+  }
+
+  render() {
+    return (
+      <div className="signup-form-parent">
+        <div className="signup-form-container">
+          <div className="signup-header-container">
+            <p className="signup-header">Sign up here!</p>
+          </div>
+          <form onSubmit={() => this.handleSubmit()}>
+            <div className="signup-field">
+              <label className="signup-label">First Name</label>
+              <input onChange={this.update('firstName')}/>
+            </div>
+            <div className="signup-field">
+              <label className="signup-label">Last Name</label>
+              <input onChange={this.update('lastName')}/>
+            </div>
+            <div className="signup-field">
+              <label className="signup-label">Username</label>
+              <input onChange={this.update('lastName')}/>
+            </div>
+            <div className="signup-field">
+              <label className="signup-label">Password</label>
+              <input onChange={this.update('lastName')}/>
+            </div>
+            <button type="submit" className="signup-submit">Submit</button>
+          </form>
+        </div>
+        <div className="signup-img">
+          <img src="https://img1.wsimg.com/isteam/ip/12c66d24-d4cb-4b8…t:0%25,l:0%25,w:100%25,h:100%25/rs=w:1023,cg:true" alt="signup-img"/>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default SignUp
+export default SignupForm;
