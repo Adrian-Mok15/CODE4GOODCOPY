8524663f34ef946ef01ffc10db790f96c930669d
roshanrajan02 <roshanrajan02@gmail.com>
2022-09-16 19:21:19 -0500

    added client

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Alumni from './pages/Alumni';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Map from './pages/Map';
+import Navigation from './components/navigation/navigation/Navigation';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navigation style={{zIndex: "1"}}/>
+        <Routes>
+          <Route className='page' path="/" element={<Home />} />
+          <Route className='page' path="/signup" element={<SignUp />} />
+          <Route className='page' path="/signin" element={<SignIn />} />
+          <Route className='page' path="/map" element={<Map />} />
+          <Route className='page' path="/alumni" element={<Alumni />} />
+          <Route  path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/navigation/navigation/constant.js
@@ -0,0 +1,30 @@
+export const routes = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "About",
+    link: "/",
+  },
+  {
+    name: "Map",
+    link: "/map",
+  },
+  {
+    name: "Alumni",
+    link: "/alumni",
+  },
+  {
+    name: "Profile",
+    link: "/profile",
+  },
+  {
+    name: "Sign Up",
+    link: "/signup",
+  },
+  {
+    name: "Contact",
+    link: "/",
+  },
+];
