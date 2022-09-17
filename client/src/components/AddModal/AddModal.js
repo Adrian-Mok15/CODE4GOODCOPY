e9332f0fbb407d261785a3cfa70a3e1753e22e74
cyyan88 <cynthiayan88@gmail.com>
2022-09-16 23:43:21 -0400

    add event modal

--- client/src/components/AddModal/AddModal.js
+++ client/src/components/AddModal/AddModal.js
@@ -1,5 +1,5 @@
 import {  useState, React  } from 'react';
-import { Modal, Button } from 'antd';
+import { Modal, Button, Form, Input, DatePicker, InputNumber } from 'antd';
 import AddEvent from '../AddEvent/AddEvent'
 
 const AddModal = () => {
@@ -17,13 +17,68 @@ const AddModal = () => {
     setIsModalOpen(false);
   };
 
+  const onFinish = (value) => {
+    console.log(value)
+    setIsModalOpen(false);
+  };
+
   return (
     <>
       <Button type="primary" onClick={showModal}>
-        Open Modal
+        Add Event +
       </Button>
       <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <AddEvent />
+      <Form
+                name="event"
+                onFinish={onFinish}
+            >
+                <Form.Item
+                    name= 'Event Name'
+                    label='Event Name'
+                    rules={[{required: true},]}
+                >
+
+                    <Input/>
+                </Form.Item>
+
+                <Form.Item
+                    name= 'Event Description'
+                    label='Event Description'
+                    rules={[{required: true},]}
+                >
+                    <Input placeholder='Event Description' />
+                </Form.Item>
+
+                <Form.Item
+                    name= 'Event Date'
+                    label='Event Date'
+                    rules={[{required: true},]}
+                    
+                >
+                    <DatePicker showTime />
+                </Form.Item>
+
+                <Form.Item
+                    name= 'Event Location'
+                    label='Event Location'
+                    rules={[{required: true},]}
+                >
+                    <Input placeholder='Event Location' />
+                </Form.Item>
+
+                <Form.Item
+                    name= 'Max Attendees'
+                    label='Max Attendees'
+                >
+                    <InputNumber />
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" >
+                        Create Event
+                    </Button>  
+                </Form.Item>
+            </Form>
       </Modal>
     </>
   );
